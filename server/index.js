ac8e744b2eea9d545db5b4700123ef32595688f9
Carlton Joseph <carlton.joseph@gmail.com>
2019-01-22 16:58:49 -0800

    server multiple file download works

--- server/index.js
+++ server/index.js
@@ -2,28 +2,40 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const multer = require("multer");
 const path = require("path");
-
 const app = express();
 const upload = multer();
+const moment = require("moment");
 
-const morgan = require("morgan");
-app.use(morgan(":method :url"));
-
+app.use((req, res, next) => {
+  console.log(moment().format("h:mm:ss"), req.method, req.url);
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../build")));
 
-app.post("/api/image-upload", upload.array("avatar", 12), (req, res) => {
+app.post("/api/file-upload", upload.single("myFile"), function(req, res, next) {
   console.log(req.file);
   console.log(req.file.buffer.toString());
-  res.send({ status: "OK" });
+  res.send({ status: "ok" });
 });
 
-app.post("/api/profile", upload.single("avatar"), function(req, res, next) {
-  console.log(req.file);
-  console.log(req.file.buffer.toString());
-  res.send({ status: "OK" });
+app.post("/api/files-upload", upload.array("myFiles", 12), (req, res, next) => {
+  const files = req.files;
+  if (!files || files.length === 0) {
+    const error = new Error("Please choose files");
+    error.httpStatusCode = 400;
+    return next(error);
+  }
+
+  console.log(files);
+
+  for (let i = 0; i < files.length; i++) {
+    console.log(files[i].buffer.toString());
+  }
+  res.send({ status: "ok" });
 });
+
 const port = process.env.PORT || 5000;
 app.listen(port);
 
